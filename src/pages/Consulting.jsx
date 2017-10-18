80bb857071ff3d32e3876a48953b8526c5637713
Monarch Wadia <monarchwadia@gmail.com>
2017-10-18 01:41:37 -0400

    Consulting page done.

--- src/pages/Consulting.jsx
+++ src/pages/Consulting.jsx
@@ -15,7 +15,7 @@ const Consulting = () => (
       technical challenges. Zero Projects is the best choice when big or complex software projects need help.
     </p>
 
-    <h3>For Startups</h3>
+    <h3>For Startups & Ventures</h3>
     <p>
       If you're a <strong>startup</strong> and need help building or growing an MVP, we can help. If you need
       assistance in building or training a team, or installing best practices, we can help there as well.
@@ -24,18 +24,29 @@ const Consulting = () => (
       through the transition.
     </p>
 
-    <h3>For Corporations</h3>
-    <p>If you're in a <strong>large corporate</strong> setting, and need a reliable team who can help quickly deliver 
-    on promises, we're your guys. Tight deadlines are no problem for us. We can also <strong>train your team</strong> to
-    work with Javascript more effectively. We also really shine when it comes to proof of concept (<strong>POC</strong>) work.
-    We make sure everything we write is reusable, maintainable and documented.
+    <h3>For Corporations & SMBs</h3>
+    <p>
+      If you're in a <strong>large corporate</strong> setting, and need a reliable team who can help quickly deliver 
+      <strong> robust business systems</strong>, we're your guys. Tight deadlines are no problem for us. We can also <strong>train your team</strong> to
+      work with Javascript more effectively. We also really shine when it comes to proof of concept (<strong>POC</strong>) work.
+      We make sure everything we write is reusable, maintainable and documented.
     </p>
 
-    <h3>For Government</h3>
-    <p></p>
+    <h3>For Government & Public Sector</h3>
+    <p>
+      If you work in the <strong>government</strong> or in <strong>public sector organizations</strong>, we can help you
+      leverage the tried-and-tested ecosystem of technologies and web frameworks that Javascript represents. We excel
+      in building <strong>responsive UI</strong> experiences that meet <strong>accessibility standards</strong> within budget
+      and on time.
+    </p>
 
-    <h3>For Non-Profits</h3>
-    <p></p>
+    <h3>For Non-Profits & Charities</h3>
+    <p>
+      We work with charities, non-profits and social enterprises to help them serve their donors and customers better.
+      Web-based user interfaces offer a world of options in <strong>engagement, outreach and fundraising</strong>. We
+      can design and develop software for <strong>mobile devices and tablets</strong> that can make fundraising easier
+      and more efficient. 
+    </p>
 
   </Page>
 )
