70cdd54750fdedae4c6e124cd47b88817dc090e8
Monarch Wadia <monarchwadia@gmail.com>
2017-10-19 20:12:29 -0400

    Finished website. repsponsiveness remains.

--- src/pages/Email.jsx
+++ src/pages/Email.jsx
@@ -7,6 +7,13 @@ const Services = () => (
     <h1> Email </h1>
 
     <p> Thanks for wanting to get in touch! We monitor our email inbox constantly. We'll get back to you really soon.</p>
+
+    <form class="email-form" action="https://formspree.io/monarch@zeroprojects.ca" method="POST">
+      <input class="text-input" type="text" name="name" placeholder="Your name" required></input>
+      <input class="text-input" type="email" name="_replyto" placeholder="Your email" required></input>
+      <textarea class="textarea-input" type="textarea" name="content" placeholder="Anything you'd like us to know"></textarea>
+      <input class="submit-button" type="submit" value="Send"></input>
+    </form>
   </Page>
 )
 
