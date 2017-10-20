346841d68e62746061db411a75c0cbd7a22e201c
Monarch Wadia <monarchwadia@gmail.com>
2017-10-20 19:39:31 -0400

    Took out router basename

--- src/index.js
+++ src/index.js
@@ -1,16 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { HashRouter, Route } from 'react-router-dom'
-import { homepage } from '../package.json'
 
 import './index.css';
 import Layout from './layout/Layout.jsx';
 import Routes from './routes.jsx';
 
-const basename = (process.env.NODE_ENV === "development") ? "/" : homepage;
-
 const Root = () => (
-  <HashRouter basename={basename}>
+  <HashRouter>
     <Layout>
       {
         Routes.map(route => (
