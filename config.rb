a123e49b97d5ed83180ffa10a6720f143af01861
ondrosh <mail@andras.io>
2019-02-06 13:34:56 -0500

    Update

--- config.rb
+++ config.rb
@@ -13,6 +13,56 @@ page '/*.xml', layout: false
 page '/*.json', layout: false
 page '/*.txt', layout: false
 
+activate :blog do |blog|
+  # This will add a prefix to all links, template references and source paths
+  blog.name = "insights"
+  blog.prefix = "insights"
+  blog.permalink = "/{title}.html"
+  blog.layout = "article_layout"
+
+  # Matcher for blog source files
+  # blog.taglink = "tags/{tag}.html"
+  # blog.summary_separator = /(READMORE)/
+  # blog.summary_length = 250
+  # blog.year_link = "{year}.html"
+  # blog.month_link = "{year}/{month}.html"
+  # blog.day_link = "{year}/{month}/{day}.html"
+  # blog.default_extension = ".markdown"
+
+  # blog.tag_template = "tag.html"
+  # blog.calendar_template = "calendar.html"
+
+  # Enable pagination
+  # blog.paginate = true
+  # blog.per_page = 10
+  # blog.page_link = "page/{num}"
+end
+
+activate :blog do |blog|
+  # This will add a prefix to all links, template references and source paths
+  blog.name = "work"
+  blog.prefix = "work"
+  blog.permalink = "/{title}.html"
+  blog.layout = "case_study_layout"
+
+  # Matcher for blog source files
+  # blog.taglink = "tags/{tag}.html"
+  # blog.summary_separator = /(READMORE)/
+  # blog.summary_length = 250
+  # blog.year_link = "{year}.html"
+  # blog.month_link = "{year}/{month}.html"
+  # blog.day_link = "{year}/{month}/{day}.html"
+  # blog.default_extension = ".markdown"
+
+  # blog.tag_template = "tag.html"
+  # blog.calendar_template = "calendar.html"
+
+  # Enable pagination
+  # blog.paginate = true
+  # blog.per_page = 10
+  # blog.page_link = "page/{num}"
+end
+
 # With alternative layout
 # page '/path/to/file.html', layout: 'other_layout'
 
